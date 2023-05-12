0d2ea6a648b734d63fb74685c0c6d0c1f5c3371f
Khushi Singh <129543601+Khushi2714@users.noreply.github.com>
2023-05-12 22:49:18 +0530

    Add files via upload

---
+++ php_projct/string.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strlen("Hello world!");
+echo "<br>";
+?> 
+
+<?php
+echo str_word_count("Hello world!");
+echo "<br>";
+cccc?> 
+
+<?php
+echo strrev("Hello world!");
+echo "<br>";
+?> 
+
+<?php
+echo strpos("Hello world!", "world");
+echo "<br>";
+?> 
+
+<?php
+echo str_replace("world", "Dolly", "Hello world!");
+echo "<br>";
+?> 
+ 
+</body>
+</html>
